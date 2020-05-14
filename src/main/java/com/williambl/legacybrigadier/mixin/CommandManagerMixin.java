961b1c6e2c8a3a6448ada9f2a29a4470bf9adb90
williambl <william@williambl.com>
2020-05-14 17:38:35 +0100

    Add help

--- src/main/java/com/williambl/legacybrigadier/mixin/CommandManagerMixin.java
+++ src/main/java/com/williambl/legacybrigadier/mixin/CommandManagerMixin.java
@@ -2,7 +2,10 @@ package com.williambl.legacybrigadier.mixin;
 
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.williambl.legacybrigadier.LegacyBrigadier;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.class_38;
+import net.minecraft.class_39;
 import net.minecraft.class_426;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,6 +13,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(class_426.class)
+@Environment(EnvType.SERVER)
 public class CommandManagerMixin {
     @Inject(
             method = "method_1411(Lnet/minecraft/class_38;)V",
@@ -21,5 +25,15 @@ public class CommandManagerMixin {
         LegacyBrigadier.dispatcher.execute(commandInfo.field_159, commandInfo.field_160);
         ci.cancel();
         } catch (CommandSyntaxException ignored) {}
+    }
+
+    @Inject(
+            method = "method_1415",
+            at = @At(value = "TAIL")
+    )
+    void appendBrigadierHelp(class_39 commandSource, CallbackInfo ci) {
+        LegacyBrigadier.dispatcher.getSmartUsage(LegacyBrigadier.dispatcher.getRoot(), commandSource).forEach((cmd, usage) -> {
+            commandSource.method_1409(String.format("   %s                        brigadier command", usage));
+                });
     }
 }
