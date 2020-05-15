41ad4da7c96c52411a2b32976a292c21b5a4683c
williambl <william@williambl.com>
2020-05-15 20:00:21 +0100

    Add /give and ItemId args

---
+++ src/main/java/com/williambl/legacybrigadier/server/api/argument/ItemId.java
@@ -0,0 +1,13 @@
+package com.williambl.legacybrigadier.server.api.argument;
+
+public class ItemId {
+    private final int id;
+
+    public ItemId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
